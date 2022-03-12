51d1965557bb58937c38e63b68db65e143afa642
kigawa <kigawa.8390@gmail.com>
2022-03-12 17:47:36 +0900

    add: database manager

--- src/main/java/net/kigawa/data/database/DataBaseManager.java
+++ src/main/java/net/kigawa/data/database/DataBaseManager.java
@@ -3,16 +3,26 @@ package net.kigawa.data.database;
 import net.kigawa.kutil.kutil.interfaces.Module;
 import net.kigawa.kutil.log.log.Logger;
 
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class DataBaseManager implements Module {
     public static Logger logger;
+    private final List<Database> databases = new LinkedList<>();
 
     public DataBaseManager(Logger logger) {
         DataBaseManager.logger = logger;
     }
 
     public Database getDatabase(String url, String name, boolean migrate) {
-        return new Database(logger, url, name, migrate);
+        var database = new Database(logger, url, name, migrate);
+        databases.add(database);
+        return database;
+    }
+
+    protected void removeDatabase(Database database) {
+        databases.remove(database);
     }
 
     @Override
@@ -22,6 +32,7 @@ public class DataBaseManager implements Module {
 
     @Override
     public void disable() {
-
+        for (var database : databases) database.close();
+        databases.clear();
     }
 }
