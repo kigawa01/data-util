ee2226529815abf7289b5a442441924bc6d9cf3d
kigawa <kigawa.8390@gmail.com>
2022-05-21 11:43:06 +0900

    feat( Mysql ): enum

--- src/main/java/net/kigawa/data/mysql/MysqlType.java
+++ src/main/java/net/kigawa/data/mysql/MysqlType.java
@@ -4,47 +4,64 @@ import java.math.BigInteger;
 
 public enum MysqlType
 {
-    TINYINT(Byte.class, "-128", "127"),
+    TINYINT("-128", "127"),
 
-    TINYINT_UNSIGNED(Short.class, "0", "255"),
-    SMALLINT(Short.class, "-32768", "32767"),
+    TINYINT_UNSIGNED("0", "255"),
+    SMALLINT("-32768", "32767"),
 
-    SMALLINT_UNSIGNED(Integer.class, "0", "65535"),
-    MEDIUMINT(Integer.class, "-8388608", "8388607"),
-    MEDIUMINT_UNSIGNED(Integer.class, "0", "16777215"),
-    INT(Integer.class, "-2147483672", "2147483647"),
-    INT_UNSIGNED(Integer.class, "0", "4294967295"),
+    SMALLINT_UNSIGNED("0", "65535"),
+    MEDIUMINT("-8388608", "8388607"),
+    MEDIUMINT_UNSIGNED("0", "16777215"),
+    INT("-2147483672", "2147483647"),
+    INT_UNSIGNED("0", "4294967295"),
 
-    BIGINT(Long.class, "-9223372036854775808", "9223372036854775807"),
+    BIGINT("-9223372036854775808", "9223372036854775807"),
 
-    BIGINT_UNSIGNED(BigInteger.class, "0", "18446744073709551615"),
+    BIGINT_UNSIGNED("0", "18446744073709551615"),
 
-    CHAR(String.class, "0", "255"),
-    VARCHAR(String.class, "0", "65535"),
-    TINYTEXT(String.class, "0", "255"),
-    TEXT(String.class, "0", "65535"),
-    MEDIUMTEXT(String.class, "0", "16777215"),
-    LONGTEXT(String.class, "0", "4294957295"),
+    FLOAT,
+    DOUBLE,
 
-    BINARY(Byte[].class, "0", "255"),
-    VARBINARY(Byte[].class, "0", "65535"),
+    DECIMAL,
+    NUMERIC,
 
-    FLOAT(Float.class),
+    BIT("1", "64"),
 
-    DOUBLE(Double.class);
+    DATE,
+    DATETIME,
+    TIMESTAMP,
+    TIME,
+    YEAR,
 
-    public final Class<?> cla;
+    CHAR("0", "255"),
+    VARCHAR("0", "65535"),
+    TINYTEXT("0", "255"),
+    TEXT("0", "65535"),
+    MEDIUMTEXT("0", "16777215"),
+    LONGTEXT("0", "4294957295"),
+
+    BINARY("0", "255"),
+    VARBINARY("0", "65535"),
+    TINYBLOB("0", "255"),
+    BLOB("0", "65535"),
+    MEDIUMBLOB("0", "16777215"),
+    LONGBLOB("0", "4294967295"),
+
+    ENUM,
+
+    SET,
+    ;
+
     public final String maxSize;
     public final String minSize;
 
-    <T> MysqlType(Class<T> cla)
+    <T> MysqlType()
     {
-        this(cla, null, null);
+        this(null, null);
     }
 
-    <T> MysqlType(Class<T> cla, String minSize, String maxSize)
+    <T> MysqlType(String minSize, String maxSize)
     {
-        this.cla = cla;
         this.maxSize = maxSize;
         this.minSize = minSize;
     }
@@ -54,13 +71,5 @@ public enum MysqlType
     {
         if (maxSize == null || minSize == null || size == null) return true;
         return new BigInteger(minSize).compareTo(size) <= 0 && size.compareTo(new BigInteger(maxSize)) <= 0;
-    }
-
-    public MysqlType getDefault(Class<?> cla, BigInteger size)
-    {
-        for (var type : values()) {
-            if (type.cla.equals(cla) && allowSize(size)) return type;
-        }
-        return null;
     }
 }
