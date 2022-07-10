1d77c1038686d3991b7fc5b5914438efb0caa080
kigawa <kigawa.8390@gmail.com>
2022-07-11 02:15:49 +0900

    feat( mysqlfield ): add fields

--- src/main/java/net/kigawa/kutil/data/mysql/FieldResolvers.java
+++ src/main/java/net/kigawa/kutil/data/mysql/FieldResolvers.java
@@ -6,8 +6,11 @@ import net.kigawa.kutil.data.exception.DatabaseException;
 import net.kigawa.kutil.data.javaField.AbstractDataField;
 import net.kigawa.kutil.data.javaField.AbstractNumberField;
 import net.kigawa.kutil.data.javaField.IntField;
+import net.kigawa.kutil.data.mysqlfield.*;
 
+import java.lang.reflect.Field;
 import java.math.BigInteger;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public enum FieldResolvers implements DatabaseResolver.ResolverInterface<AbstractDataField, DatabaseField>
@@ -28,26 +31,29 @@ public enum FieldResolvers implements DatabaseResolver.ResolverInterface<Abstrac
 
     private enum NumberFieldRange
     {
-        TINYINT("-128", "127"),
-        UNREGISTERED_TINYINT("0", "255"),
-        SMALL_INT("-32768", "32767"),
-        UNREGISTERED_SMALLINT("0", "65535"),
-        MEDIUMINT("-8388608", "8388607"),
-        UNREGISTERED_MEDIUMINT("0", "16777215"),
-        INT("-2147483648", "2147483647"),
-        UNREGISTERED_INT("0", "4294967295"),
-        BIGINT("-9223372036854775808", "9223372036854775807"),
-        UNREGISTERED_BIGINT("0", "18446744073709551615"),
+        TINYINT("-128", "127", TinyInt::new),
+        UNREGISTERED_TINYINT("0", "255", UnregisteredTinyInt::new),
+        SMALL_INT("-32768", "32767", SmallInt::new),
+        UNREGISTERED_SMALLINT("0", "65535", UnregisteredSmallInt::new),
+        MEDIUMINT("-8388608", "8388607", MediumInt::new),
+        UNREGISTERED_MEDIUMINT("0", "16777215", UnregisteredMediumInt::new),
+        INT("-2147483648", "2147483647", MysqlInt::new),
+        UNREGISTERED_INT("0", "4294967295", UnregisteredInt::new),
+        BIGINT("-9223372036854775808", "9223372036854775807", BigInt::new),
+        UNREGISTERED_BIGINT("0", "18446744073709551615", UnregisteredBigInt::new),
         ;
 
         public final BigInteger min;
         public final BigInteger max;
+        public final BiConsumer<AbstractDataField, Field> createDatabaseField;
 
-        NumberFieldRange(String min, String max)
+        NumberFieldRange(String min, String max, BiConsumer<AbstractDataField, Field> createDatabaseField)
         {
             this.min = new BigInteger(min);
             this.max = new BigInteger(max);
+            this.createDatabaseField = createDatabaseField;
         }
+
     }
 
     private final Class<? extends AbstractDataField> javaFieldClass;
---
+++ src/main/java/net/kigawa/kutil/data/mysqlfield/UnregisteredInt.java
@@ -0,0 +1,40 @@
+package net.kigawa.kutil.data.mysqlfield;
+
+import net.kigawa.kutil.data.javaField.AbstractDataField;
+import net.kigawa.kutil.data.sql.SqlBuilder;
+
+import java.lang.reflect.Field;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UnregisteredInt extends MysqlField
+{
+    public UnregisteredInt(AbstractDataField dataField, Field field)
+    {
+        super(dataField, field);
+    }
+
+    @Override
+    public String getTypeName()
+    {
+        return null;
+    }
+
+    @Override
+    public void writeStatement(PreparedStatement statement, int index)
+    {
+
+    }
+
+    @Override
+    public void readResult(ResultSet result)
+    {
+
+    }
+
+    @Override
+    protected void configureOptions(SqlBuilder sqlBuilder)
+    {
+
+    }
+}
